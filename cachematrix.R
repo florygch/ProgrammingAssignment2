4593caa67cf581554397cd9d6a858e564c7debcd
florygch <69543076+florygch@users.noreply.github.com>
2020-09-08 18:44:52 -0600

    Update Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assigment 2
+## R Programming Coursera
 
-## Write a short comment describing this function
+## Floribel Gonzalez Chacon
 
-makeCacheMatrix <- function(x = matrix()) {
+## Create matrix example with aleatory numbers
+matriz_ex<-matrix(sample(1:500,100),10,10)
 
+## makeCacheMatrix Function
+## x is a matrix, I assigned a default value of matrix 10x10 aleatory
+## output: list with 4 elements:set,get,setsolve, getsolve, 
+
+makeCacheMatrix <- function(x = matrix(sample(1:500,100),10,10)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() 
+    x
+  setsolve <- function(solve) 
+    s <<- solve
+  getsolve <- function() 
+    s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve Function
+## the function try to get the inverse matriz
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
